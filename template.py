257fe7973b2e0c720f6777ee2b969d6412aadcaf
Amit <axk173030@utdallas.edu>
2025-08-04 15:01:02 -0500

    logs added

--- template.py
+++ template.py
@@ -6,6 +6,7 @@ logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
 project_name="mlProject"
 list_of_files=[
     ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
